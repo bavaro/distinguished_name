32b4d22162faebdfe09a1644e3780c5c6091caff
Midpoint Data Layer <info@kestrelei.com>
2012-12-04 08:41:18 -0500

    require_relative hack to make this gem compatible with earlier version fo ruby

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,7 @@
 require 'test/unit'
-require_relative "../lib/distinguished_name"
+
+if Kernel.respond_to?(:require_relative)
+  require_relative "../lib/distinguished_name"
+else
+  require File.join(File.dirname(caller[0]), "../lib/distinguished_name")
+end
